d055db571e76a3355720e8bce4ad55e70cbe103b
sabbir1257 <masabbir1257@gmail.com>
2025-05-09 11:56:36 +0600

    HeadlessUI and initial generic modal setup

---
+++ src/components/ui/Modal.jsx
@@ -0,0 +1,33 @@
+import { Dialog } from "@headlessui/react";
+
+export default function Modal({ isOpen, setIsOpen }) {
+  const close = () => setIsOpen(false);
+
+  return (
+    <>
+      <Dialog open={isOpen} onClose={close} className="relative z-10">
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-black/50">
+          <div className="flex items-center justify-center min-h-full p-4">
+            <Dialog.Panel className="w-full max-w-md p-6 bg-white shadow-xl rounded-xl">
+              <Dialog.Title className="text-lg font-medium text-gray-900">
+                Payment successful
+              </Dialog.Title>
+              <p className="mt-2 text-sm text-gray-600">
+                Your payment has been successfully submitted. We’ve sent you an
+                email with all the details.
+              </p>
+              <div className="mt-4">
+                <button
+                  onClick={close}
+                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm font-semibold text-white hover:bg-gray-600"
+                >
+                  Got it, thanks!
+                </button>
+              </div>
+            </Dialog.Panel>
+          </div>
+        </div>
+      </Dialog>
+    </>
+  );
+}
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,28 +1,38 @@
-import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import MyTasks from '../components/tasks/MyTasks';
-import TaskCard from '../components/tasks/TaskCard';
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import MyTasks from "../components/tasks/MyTasks";
+import TaskCard from "../components/tasks/TaskCard";
+import Modal from "../components/ui/Modal.jsx";
+import { useState } from "react";
 
 const Tasks = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="h-screen grid grid-cols-12">
+    <div className="grid h-screen grid-cols-12">
       <div className="col-span-9 px-10 pt-10">
-        <div className="flex justify-between items-center">
+        <div className="flex items-center justify-between">
           <div>
-            <h1 className="font-semibold text-3xl">Tasks</h1>
+            <h1 className="text-3xl font-semibold">Tasks</h1>
           </div>
           <div className="flex gap-5">
-            <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10  grid place-content-center text-secondary hover:text-white transition-all">
-              <MagnifyingGlassIcon className="h-6 w-6" />
+            <button className="grid w-10 h-10 transition-all border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl place-content-center text-secondary hover:text-white">
+              <MagnifyingGlassIcon className="w-6 h-6" />
             </button>
-            <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10 grid place-content-center text-secondary hover:text-white transition-all">
-              <BellIcon className="h-6 w-6" />
+            <button className="grid w-10 h-10 transition-all border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl place-content-center text-secondary hover:text-white">
+              <BellIcon className="w-6 h-6" />
             </button>
-            <button className="btn btn-primary">Add Task</button>
-            <div className="h-10 w-10 rounded-xl overflow-hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="btn btn-primary"
+            >
+              Add Task
+            </button>
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+            <div className="w-10 h-10 overflow-hidden rounded-xl">
               <img
                 src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80"
                 alt=""
-                className="object-cover h-full w-full "
+                className="object-cover w-full h-full "
               />
             </div>
           </div>
@@ -31,7 +41,7 @@ const Tasks = () => {
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
               <h1>Up Next</h1>
-              <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
+              <p className="grid w-6 h-6 text-white rounded-md bg-primary place-content-center">
                 0
               </p>
             </div>
@@ -42,7 +52,7 @@ const Tasks = () => {
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
               <h1>In Progress</h1>
-              <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
+              <p className="grid w-6 h-6 text-white rounded-md bg-primary place-content-center">
                 0
               </p>
             </div>
@@ -54,7 +64,7 @@ const Tasks = () => {
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
               <h1>Up Next</h1>
-              <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
+              <p className="grid w-6 h-6 text-white rounded-md bg-primary place-content-center">
                 0
               </p>
             </div>
@@ -64,43 +74,43 @@ const Tasks = () => {
           </div>
         </div>
       </div>
-      <div className="col-span-3 border-l-2 border-secondary/20 px-10 pt-10">
+      <div className="col-span-3 px-10 pt-10 border-l-2 border-secondary/20">
         <div>
           <h1 className="text-xl">Members</h1>
           <div className="flex gap-3 mt-3">
-            <div className="h-10 w-10 rounded-xl overflow-hidden">
+            <div className="w-10 h-10 overflow-hidden rounded-xl">
               <img
                 src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
                 alt=""
-                className="object-cover h-full w-full "
+                className="object-cover w-full h-full "
               />
             </div>
-            <div className="h-10 w-10 rounded-xl overflow-hidden">
+            <div className="w-10 h-10 overflow-hidden rounded-xl">
               <img
                 src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
                 alt=""
-                className="object-cover h-full w-full "
+                className="object-cover w-full h-full "
               />
             </div>
-            <div className="h-10 w-10 rounded-xl overflow-hidden">
+            <div className="w-10 h-10 overflow-hidden rounded-xl">
               <img
                 src="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
                 alt=""
-                className="object-cover h-full w-full "
+                className="object-cover w-full h-full "
               />
             </div>
-            <div className="h-10 w-10 rounded-xl overflow-hidden">
+            <div className="w-10 h-10 overflow-hidden rounded-xl">
               <img
                 src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
                 alt=""
-                className="object-cover h-full w-full "
+                className="object-cover w-full h-full "
               />
             </div>
-            <div className="h-10 w-10 rounded-xl overflow-hidden">
+            <div className="w-10 h-10 overflow-hidden rounded-xl">
               <img
                 src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
                 alt=""
-                className="object-cover h-full w-full "
+                className="object-cover w-full h-full "
               />
             </div>
           </div>
